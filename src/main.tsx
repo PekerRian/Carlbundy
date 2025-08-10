eb6ff91d50f262f34d3b113234de51436bd0b70c
PEKER <adrianneveluz@gmail.com>
2025-08-11 07:35:12 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { PetraWallet } from 'petra-plugin-wallet-adapter';
 import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
 import { Network } from '@aptos-labs/ts-sdk';
 import App from './App';
@@ -13,17 +12,11 @@ if (!container) {
 
 const root = ReactDOM.createRoot(container);
 
-const wallets = [new PetraWallet()];
-
 root.render(
   <React.StrictMode>
     <AptosWalletAdapterProvider 
-      plugins={wallets}
       autoConnect={true}
-      dappConfig={{
-        network: Network.TESTNET,
-      }}
-      onError={(error) => {
+      onError={(error: Error) => {
         console.error("Wallet adapter error:", error);
       }}
     >
