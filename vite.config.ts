a6f22a859638ce3ce97d6f6157755db7f6266d8a
PEKER <adrianneveluz@gmail.com>
2025-08-11 08:24:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,19 +11,17 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020'
-    },
-    include: ['petra-plugin-wallet-adapter']
+    }
   },
   build: {
     target: 'es2020',
     commonjsOptions: {
-      include: [/petra-plugin-wallet-adapter/, /node_modules/]
+      include: [/node_modules/]
     },
     rollupOptions: {
-      external: ['@telegram-apps/bridge'],
       output: {
         manualChunks: {
-          vendor: ['petra-plugin-wallet-adapter', '@aptos-labs/wallet-adapter-react']
+          vendor: ['@aptos-labs/wallet-adapter-react', '@aptos-labs/wallet-adapter-core']
         }
       }
     }
